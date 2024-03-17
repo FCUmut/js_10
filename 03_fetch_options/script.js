3c1c8f605df8a3716f8e0b925ed0c48abb84b5ce
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-03-17 23:52:32 +0300

    commit
    commit

---
+++ 03_fetch_options/script.js
@@ -0,0 +1,20 @@
+// function createPost(post) {
+function createPost({ title, body }) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      //   title: post.title,
+      //   body: post.body,
+      title,
+      body,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      token: "abc123",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+createPost({ title: "My Post", body: "This is my post" });
